3fe7aa0c7a8f9c2895174f41435bdda3034b013e
Devesh Lashkari <deveshlashkari@gmail.com>
2024-05-29 12:48:59 +0530

    initial commit

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import Invoices from "../components/Invoices";
+
+
+function App() {
+    return (
+        <div className="p-5">
+            <Invoices />
+        </div>
+    );
+}
+export default App;
